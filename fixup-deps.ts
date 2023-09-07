99e93c2ff8ab8cd0f937e2c4671a1defc0e3d385
Brian Stoker <stoked@newCaladan>
2023-09-07 04:22:16 -0500

    bump

--- fixup-deps.ts
+++ fixup-deps.ts
@@ -1,3 +1,7 @@
 import { Fixup } from '@stoked-cenv/lib';
+import { join } from 'path';
 
-Fixup.updateDeps(Fixup.loadMetas(Fixup.packageRoot, Fixup.allPackages));
+const workspaceMetas = Fixup.loadMetas(Fixup.packageRoot, Fixup.allPackages);
+const metaObj = Fixup.loadMeta(join(process.cwd(), 'package.json'));
+workspaceMetas[metaObj.meta.name] = metaObj;
+Fixup.updateDeps(workspaceMetas);
