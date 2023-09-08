e69f167740caf45765da8d8644237350f3ffd3fa
Brian Stoker <stoked@newCaladan>
2023-09-07 19:43:10 -0500

    add task and service launchers

--- packages/cdk/src/index.ts
+++ packages/cdk/src/index.ts
@@ -4,6 +4,8 @@ export const iam = aws_iam;
 export * from 'aws-cdk-lib';
 export * from './ecs/ecs';
 export * from './ecs/ecs-job';
+export * from './ecs/fargate.task';
+export * from './ecs/fargate.service';
 export * from './python-lambda';
 export * from './lambda-props';
 export * from './utils';
